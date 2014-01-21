3e63150d9e8c11c9888d2a1616acae512ecab824
Prownageable <jeroensteendam@gmail.com>
2014-01-21 16:02:38 +0100

    Organised code
    Added AsyncTasks for webrequests

--- src/com/example/rtpodcasts/MainActivity.java
+++ src/com/example/rtpodcasts/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.rtpodcasts;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
+import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -12,9 +12,28 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		PodcastHandler ph = new PodcastHandler();
-		Log.d("tga", "There are " + ph.GetPatchEpisodes().size() + " episodes of The Patch!");
-		Log.d("tga", "There are " + ph.GetPodcastEpisodes().size() + " episodes of The RT Podcast!");
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+		StrictMode.setThreadPolicy(policy);
+
+		new Thread(new PodcastHandler()).start();
+//		new Runnable() {
+//			@Override
+//			public void run() {
+//				try {
+//					EditText input = (EditText) findViewById(R.id.inputField);
+//					TextView output = (TextView) findViewById(R.id.outputField);
+//					String escaped = JSONObject.quote(input.getText().toString());
+//					Log.d("app", escaped);
+//					String result = new RequestTask().execute("http://coliru.stacked-crooked.com/compile", escaped).get();
+//					output.setText(result);
+//				} catch (InterruptedException e) {
+//				} catch (ExecutionException e) {
+//				}
+//			}
+//		}.run();
+//		Log.d("tga", "There are " + ph.GetPatchEpisodes().size() + " episodes of The Patch!");
+//		Log.d("tga", "There are " + ph.GetPodcastEpisodes().size() + " episodes of The RT Podcast!");
 	}
 
 	@Override
--- src/com/example/rtpodcasts/PodcastHandler.java
+++ src/com/example/rtpodcasts/PodcastHandler.java
@@ -1,14 +1,13 @@
 package com.example.rtpodcasts;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,111 +16,148 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-public class PodcastHandler {
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class PodcastHandler implements Runnable {
+
 	public PodcastHandler() {
-		GrabPatchXML();
-		GrabPodcastXML();
 	}
 
-	private void GrabPatchXML() {
-		try {
-			URL website = new URL("http://s3.roosterteeth.com/podcasts/gaming-mp3.xml");
-			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-			FileOutputStream fos = new FileOutputStream("gaming.xml");
-			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-		} catch (IOException e) {
+	private class PatchGrabber extends AsyncTask<String, String, String> {
+		private String GrabPatchXML() {
+			try {
+				URL url = new URL("http://s3.roosterteeth.com/podcasts/gaming-mp3.xml");
+				BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+				String str;
+				String xml = "";
+				while ((str = in.readLine()) != null) {
+					xml += str;
+				}
+				in.close();
+				return xml;
+			} catch (IOException e) {
+				Log.e("tga", e.toString());
+			}
+			return null;
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			return GrabPatchXML();
 		}
 	}
 
-	private void GrabPodcastXML() {
-		try {
-			URL website = new URL("http://s3.roosterteeth.com/podcasts/mp3.xml");
-			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-			FileOutputStream fos = new FileOutputStream("podcast.xml");
-			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-		} catch (IOException e) {
+	private class PodcastGrabber extends AsyncTask<String, String, String> {
+		private String GrabPodcastXML() {
+			try {
+				URL url = new URL("http://s3.roosterteeth.com/podcasts/mp3.xml");
+				BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+				String str;
+				String xml = "";
+				while ((str = in.readLine()) != null) {
+					xml += str;
+				}
+				in.close();
+				return xml;
+			} catch (IOException e) {
+				Log.e("tga", e.toString());
+			}
+			return null;
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			return GrabPodcastXML();
 		}
 	}
 
-	public List<Podcast> GetPatchEpisodes() {
-		return GetEpisodes("gaming.xml");
-	}
-
-	public List<Podcast> GetPodcastEpisodes() {
-		return GetEpisodes("podcast.xml");
-	}
+	// private List<Podcast> GetPatchEpisodes() {
+	// return GetEpisodes(GrabPatchXML());
+	// }
+	//
+	// private List<Podcast> GetPodcastEpisodes() {
+	// return GetEpisodes(GrabPodcastXML());
+	// }
 
 	private List<Podcast> GetEpisodes(String file) {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = null;
-
 		try {
 			builder = factory.newDocumentBuilder();
 		} catch (ParserConfigurationException e) {
 		}
-
-		// Load and Parse the XML document
-		// document contains the complete XML as a Tree.
 		Document document = null;
 		try {
-			File f = new File(file);
-			FileInputStream fis = new FileInputStream(f);
-			document = builder.parse(fis);
-		} catch (IOException e) {
-		} catch (SAXException e) {
+			document = builder.parse(new ByteArrayInputStream(file.getBytes()));
+		} catch (Exception e) {
+			Log.e("tga", e.toString());
 		}
 
-		List<Podcast> episodeList = new ArrayList<>();
-		// Iterating through the nodes and extracting the data.
+		ArrayList<Podcast> episodeList = new ArrayList<Podcast>();
+		if (document != null) {
+			NodeList nodeList = document.getDocumentElement().getChildNodes();
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				Node root = nodeList.item(i);
+				NodeList rootlist = root.getChildNodes();
 
-		NodeList nodeList = document.getDocumentElement().getChildNodes();
-		for (int i = 0; i < nodeList.getLength(); i++) {
-			// We have encountered an <employee> tag.
-			Node root = nodeList.item(i);
-			NodeList rootlist = root.getChildNodes();
+				for (int j = 0; j < rootlist.getLength(); j++) {
+					Node item = rootlist.item(j);
 
-			for (int j = 0; j < rootlist.getLength(); j++) {
-				// System.out.println(rootlist.item(j).getNodeName());
-				Node item = rootlist.item(j);
-
-				if (item.getNodeName().equals("item")) {
-					Podcast podcast = new Podcast();
-					NodeList subItemList = item.getChildNodes();
-					for (int k = 0; k < subItemList.getLength(); k++) {
-						Node subItem = subItemList.item(k);
-						switch (subItem.getNodeName()) {
-						case "title":
-							podcast.setTitle(subItem.getTextContent());
-							break;
-						case "link":
-							podcast.setWeblink(subItem.getTextContent());
-							break;
-						case "itunes:author":
-							podcast.setAuthor(subItem.getTextContent());
-							break;
-						case "pubDate":
-							podcast.setDate(subItem.getTextContent());
-							break;
-						case "description":
-							break;
-						case "enclosure":
-							String s = subItem.getAttributes().item(1).toString();
-							podcast.setDownload_url(s.substring(5, s.length() - 1));
-							break;
-						case "guid":
-							podcast.setItem_nr(subItem.getTextContent());
-							break;
-						case "itunes:duration":
-							podcast.setDuration(subItem.getTextContent());
-							break;
+					if (item.getNodeName().equals("item")) {
+						Podcast podcast = new Podcast();
+						NodeList subItemList = item.getChildNodes();
+						for (int k = 0; k < subItemList.getLength(); k++) {
+							Node subItem = subItemList.item(k);
+							String name = subItem.getNodeName();
+							if (name.equals("title")) {
+								podcast.setTitle(subItem.getTextContent());
+							} else if (name.equals("link")) {
+								podcast.setWeblink(subItem.getTextContent());
+							} else if (name.equals("itunes:author")) {
+								podcast.setAuthor(subItem.getTextContent());
+							} else if (name.equals("pubDate")) {
+								podcast.setDate(subItem.getTextContent());
+							} else if (name.equals("description")) {
+							} else if (name.equals("enclosure")) {
+								String s = subItem.getAttributes().item(1).toString();
+								podcast.setDownload_url(s.substring(5, s.length() - 1));
+							} else if (name.equals("guid")) {
+								podcast.setItem_nr(subItem.getTextContent());
+							} else if (name.equals("itunes:duration")) {
+								podcast.setDuration(subItem.getTextContent());
+							}
 						}
+						episodeList.add(podcast);
 					}
-					episodeList.add(podcast);
 				}
 			}
 		}
 		return episodeList;
+	}
+
+	@Override
+	public void run() {
+		for (;;) {
+			String podcasts = null;
+			String patch = null;
+			try {
+				podcasts = new PodcastGrabber().execute("").get();
+				patch = new PatchGrabber().execute("").get();
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			} catch (ExecutionException e1) {
+				e1.printStackTrace();
+			}
+			if(patch != null)
+				Log.d("tga", "There are " + GetEpisodes(patch).size() + " episodes of The Patch!");
+			if(podcasts != null)
+				Log.d("tga", "There are " + GetEpisodes(podcasts).size() + " episodes of The RT Podcast!");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+		}
 	}
 }
